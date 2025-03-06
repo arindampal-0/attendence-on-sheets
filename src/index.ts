5c21e79a487f44622da527702f94d0df81c46d32
Arindam Pal <arindampal2000@gmail.com>
2025-03-06 17:36:54 +0530

    A simple fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({ logger: true });
+const PORT = Number(process.env.PORT) || 3000;
+
+fastify.get("/", function (_, reply) {
+  reply.send({ msg: "Hello, World!" });
+});
+
 async function main() {
-    console.log("Hello, World!");
+    fastify.listen({ port: PORT, host: "0.0.0.0"}, function(err, address) {
+        if (err) {
+            fastify.log.error(err);
+            process.exit(1);
+        }
+
+        console.log(`Server started at ${address}`);
+    });
 }
 
 main().catch((err) => console.error(err));
