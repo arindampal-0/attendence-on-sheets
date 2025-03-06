3cab93bf7345f6d3357efbaef205e3897e46ee63
Arindam Pal <arindampal2000@gmail.com>
2025-03-06 19:09:59 +0530

    fix: serverless function in ts

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,17 +1,15 @@
-"use strict";
 
-// import * as dotenv from "dotenv";
-// dotenv.config();
+"use strict";
 
 import Fastify from "fastify";
 
-import apiRoutes from "../dist/api";
+import apiRoutes from "./api.js";
 
 const app = Fastify({ logger: true });
 
 app.register(apiRoutes);
 
-export default async function(req, res) {
+export default async function(req: Request, res: Response) {
     await app.ready();
     app.server.emit("request", req, res);
 }
