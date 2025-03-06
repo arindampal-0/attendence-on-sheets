7942a14a041ccc267f125a8d020d86a36dac7a3e
Arindam Pal <arindampal2000@gmail.com>
2025-03-06 19:58:17 +0530

    update rewrite rule trial 4

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,9 @@
 import type { FastifyPluginAsync } from "fastify";
 
 const apiRoutes: FastifyPluginAsync = async function (fastify) {
+  fastify.get("/", async function (_, reply) {
+    reply.send({ msg: "Hello, World!" });
+  });
   fastify.get("/health", async function (_, reply) {
     reply.send({ health: "ok" });
   });
