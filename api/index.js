c3b1b3b033e44bbded437f6f8e4324e051c3e41e
Arindam Pal <arindampal2000@gmail.com>
2025-03-06 19:27:19 +0530

    revert to api directory

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
-
 "use strict";
 
 import Fastify from "fastify";
 
-import apiRoutes from "./api.js";
+import apiRoutes from "../dist/api.js";
 
 const app = Fastify({ logger: true });
 
 app.register(apiRoutes);
 
-export default async function(req: Request, res: Response) {
+export default async function(req, res) {
     await app.ready();
     app.server.emit("request", req, res);
 }
