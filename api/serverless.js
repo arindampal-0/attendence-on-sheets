086341ef1cae6a329a73cabcee8ed97c485b4a48
Arindam Pal <arindampal2000@gmail.com>
2025-03-06 17:57:56 +0530

    vercel config for static site and api

---
+++ api/serverless.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import Fastify from "fastify";
+
+import apiRoutes from "../dist/api";
+
+const app = Fastify({ logger: true });
+
+app.register(apiRoutes);
+
+export default async function(req, res) {
+    await app.ready();
+    app.server.emit("request", req, res);
+}
